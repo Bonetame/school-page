9f671e3d1ec738e9b06c91b3ea2a465652e59846
Bonetame <101095369+Bonetame@users.noreply.github.com>
2026-04-30 09:22:41 +0300

    PUSH

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector("button").addEventListener("click", function() {
     alert("Welcome to My School!");
 });
+document.querySelector(".contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Message sent successfully!");
+});
